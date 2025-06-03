78ba59fc0905eea3645e124280a370f19e6de034
Vlad Solomon <vladsolomon720@gmail.com>
2025-06-03 14:46:56 +0300

    add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import "./index.css";
 
 import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 );
